fff163d85f8537b3ae609fa3f657d87c5099bc34
Saumya <swarupsaumya65@gmail.com>
2024-08-05 00:16:46 +0530

    fix deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import NotFound from "./components/pages/NotFound";
 import LandingPage from "./components/pages/LandingPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <LandingPage />
   );
 }
 
